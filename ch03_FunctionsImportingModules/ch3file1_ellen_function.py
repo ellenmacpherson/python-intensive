486af2bf17b593c10e151d91d4f45bce49b4f672
Ellen Macpherson <macphersonellen@outlook.com>
2018-12-19 15:36:43 +0000

    finalising changes

--- ch03_FunctionsImportingModules/ch3file1_ellen_function.py
+++ ch03_FunctionsImportingModules/ch3file1_ellen_function.py
@@ -1,47 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 19 15:21:10 2018
 
-"""---------------------------Introduction to functions------------------------"""
-
-#------------------------------BASIC EXAMPLE FUNCTIONS------------------------------
-
+@author: ellenmacpherson
+"""
+###############################################################################
+######################### Basic functions ################################
+###############################################################################
 
 def birthday_message_2args(a, b):
     print('{} {}'.format(a, b))
 
-var1 = 'Happy'
-var2 = 'Birthday!'
-var3 = 'You\'re 26 years old!'
-var4 = 'Oh dear.'
-
-birthday_message_2args(var1, var2)
-birthday_message_2args(var3, var4)
-
-
-
-
 def hello_world_4args(a, b, c, d):
     print ('{} {} {} {}'.format(a, b, c, d))
-
-a1 = 'Hello'
-b1 = 'world!'
-a2 = 'I love'
-b2 = 'coding!'
-c1 = 'Python'
-d1 = 'is'
-c2 = 'the'
-d2 = 'best.'
-hello_world_4args(a1, b1, a2, b2)
-hello_world_4args(c1, d1, c2, d2)
-
-
-
+    
+def addition(): #Adds 2+2
+    four = 2 + 2
+    print ('Your fun fact for today is...\n2 + 2 is {}!'.format(four))
+    
 def add_two_numbers(n1, n2):
     answer = n1 + n2
     return (str(n1) + ' plus ' + str(n2) + ' is ' + str(answer))
 
-
-#---------------------------------MORE COMPLEX FUNCTIONS------------------------------------
+###############################################################################
+######################### Converting functions ################################
+###############################################################################
 
 def convert_distance(miles):
     kilometers = (miles * 8.0) / 5.0
@@ -63,23 +47,10 @@ def temperature_conversion(centigrade):
     fahrenheit = ((kelvin1 * 9) / 5) - 459.67
     return ('{} degrees is equal to {} degrees in kelvin and {} degrees in fahrenheit.'.format(centigrade, kelvin1, fahrenheit))
 
-#--------------------------------INPUTS IN functions-----------------------------------------
+###############################################################################
+######################### Functions with Input ################################
+###############################################################################
 
-#Exploring what you can do with the input function
-name = input('What\'s your name? ')
-print ('Hello {}!'.format(name.title())) #Prints 'Hello Name' to console. .title ensures the first letter of name is capitalised.
-
-age = input('How old are you? ')
-print ('You\'re {}. Thanks for letting us know!'.format(age))
-
-homeCountry = input('Where are you from? ')
-print ('You\'re from {}? Wow!'.format(homeCountry.title()))
-
-#Writing functions
-
-def addition(): #Adds 2+2
-    four = 2 + 2
-    print ('Your fun fact for today is...\n2 + 2 is {}!'.format(four))
 
 def printName(): #Asks for name input, then prints name in title format and 'fun fact' addition function.
     name = input('Enter your name: ')
--- ch03_FunctionsImportingModules/ch3file2_ellen_test.py
+++ ch03_FunctionsImportingModules/ch3file2_ellen_test.py
@@ -7,4 +7,27 @@ Created on Thu Nov 29 13:58:32 2018
 """
 #Working with imported functions.
 
-from ch3file1_ellen_function import mathsInputs
+#Importing functions from external files. 
+from ch3file1_ellen_function import * 
+
+
+#Exploring what you can do with the input function
+name = input('What\'s your name? ')
+print ('Hello {}!'.format(name.title())) #Prints 'Hello Name' to console. .title ensures the first letter of name is capitalised.
+
+age = input('How old are you? ')
+print ('You\'re {}. Thanks for letting us know!'.format(age))
+
+homeCountry = input('Where are you from? ')
+print ('You\'re from {}? Wow!'.format(homeCountry.title()))
+
+
+#Calling functions and user input
+
+helloWorld()
+
+#Task 5
+centigrade = int(input('Temperature in centigrade: '))
+
+temperature_conversion(centigrade)
+kelvin_converter(centigrade)
